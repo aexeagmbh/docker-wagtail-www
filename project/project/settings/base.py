d4923d6c56bb988d2e588cea6d974ae96f6c290d
Arne Schauf <git@as-webservices.de>
2015-04-10 14:12:12 +0200

    fix db name

--- project/project/settings/base.py
+++ project/project/settings/base.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
+        'NAME': 'wagtail',
         'USER': 'postgres',
         'PASSWORD': '',
         'HOST': 'db',  # Set to empty string for localhost.
