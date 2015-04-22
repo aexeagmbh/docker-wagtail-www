d9d054f1bf7d61e7f2336eba1002085c112e699d
EugenSchaffner <es056@hdm-stuttgart.de>
2015-04-22 09:37:47 +0200

    edited click handler for animation of employee container

--- project/core/static/js/teampage.js
+++ project/core/static/js/teampage.js
@@ -1,19 +1,68 @@
 $(document).ready(function() {
 
-	$(".arrow-icon").click(function() {
-		var deg=-90;
-
-		$(this).children("img").animate( {
-
-                   transform: 'rotate('+deg+'deg)'
-		},1000);
-
-		$(this).parents(".employee").children(".name , .jobtitle, .telephone , .email").animate({
-			display:'initial',
-			
-			top:'-11rem'
-
-		},1000);
-	});
+	var animateElements = function(that) {
+		event.preventDefault();
 	
+		
+		var offset = 14;
+		var elements = that.parents(".employee").children(" .jobtitle, .email , .telephone");
+		elements.stop();
+		if (that.hasClass("isDown")) {
+				var d = 0;
+
+		that.children("img").css(
+		{
+
+			'-moz-transform': 'rotate(' + d + 'deg)',
+			'-webkit-transform': 'rotate(' + d + 'deg)',
+			'-o-transform': 'rotate(' + d + 'deg)',
+			'-ms-transform': 'rotate(' + d + 'deg)',
+			'transform': 'rotate(' + d + 'deg)'
+
+		});
+			elements = elements.map(function() {
+				return $(this).animate({
+					bottom: '6rem'
+				}, 1000)
+			})
+
+		} else {
+
+				var d = -90;
+
+		that.children("img").css(
+		{
+
+			'-moz-transform': 'rotate(' + d + 'deg)',
+			'-webkit-transform': 'rotate(' + d + 'deg)',
+			'-o-transform': 'rotate(' + d + 'deg)',
+			'-ms-transform': 'rotate(' + d + 'deg)',
+			'transform': 'rotate(' + d + 'deg)'
+
+		});
+			elements = elements.map(function() {
+
+				offset -= 2;
+				return $(this).animate({
+
+
+					bottom: offset + 'rem'
+
+				}, 1000)
+			});
+
+		}
+
+		that.toggleClass("isDown");
+		return false;
+
+	}
+	$(".arrow-icon").click(function() {
+
+		animateElements($(this));
+
+	})
+
+
+
 });
