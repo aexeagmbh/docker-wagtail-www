f1580a99de5c170143747ade47e9d26544204818
eugenSchaffner <eugen.schaffner@googlemail.com>
2015-04-23 12:35:23 +0200

    adjusted animations offset

--- project/core/static/js/teampage.js
+++ project/core/static/js/teampage.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
 		if (that.children().hasClass('telephone')) {
 
 			var elements = that.children(" .email , .telephone");
-			var offset = 9.4 + 4;
+			var offset = 9.3 + 4;
 		} else {
 			var elements = that.children(".email");
 			elements.css("background-color", "#DDDDDD")
-			var offset = 9.4 + 2;
+			var offset = 9.3 + 2;
 		}
 		elements.stop();
 		if (that.hasClass("isDown")) {
